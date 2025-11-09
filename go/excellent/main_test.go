8850411c129e94334c5bc94a9461f479fd80ad74
saketoro <information.lab0309@gmail.com>
2025-11-09 10:41:01 +0900

    [fix] Add EvenOrOdd function and its test

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -2,7 +2,7 @@ package main
 import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
-	result := EvenOrOdd(3)
+	result := EvenOrOdd(4)
 	if result != "Even" {
 		t.Errorf("Expected 'Even', but got '%s'", result)
 	}
